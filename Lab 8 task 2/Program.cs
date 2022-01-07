8a0ad1527bd0d4c468200dbb126afc484114a4db
XetT62rus <michul@mail.u>
2022-01-07 17:44:20 +0300

    Добавьте файлы проекта.

---
+++ Lab 8 task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Log/Директории.txt";
+            StreamWriter sw = new StreamWriter(path);
+            int[] mass = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                mass[i] = random.Next();
+                sw.WriteLine("{0}", mass[i]);                
+            }
+            sw.Close();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "H:\\Visual Studio проект";
+            string [] drives = Directory.GetDirectories(path);
+            foreach (string folder in drives)
+            {
+                Console.WriteLine(folder);
+            }
+            Console.ReadKey();
+        }
+    }
+}
